7462008cbc6cfd2297edfbf8ac13f7e9b847c2e1
enzoprog <e.ferreiradealmeida@rubika-edu.com>
2022-09-26 16:31:19 +0200

    Player controller V1 avec deplacements et dash

---
+++ SI/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    public SO_Controller SO_Controller;
+
+    [SerializeField] private Rigidbody2D m_rigidbody;
+    [SerializeField] private float m_timerDash = 0f;
+    
+    private void Awake()
+    {
+        instance = this;
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void ResetVelocity()
+    {
+        m_rigidbody.velocity = Vector2.zero;
+    }
+
+    private void Start()
+    {
+        ReInit();
+    }
+    
+    public void ReInit()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && m_timerDash < -0.5f)
+        {
+            m_timerDash = SO_Controller.m_durationDash;
+        }
+        
+        m_timerDash -= Time.deltaTime;
+
+    }
+
+    public void FixedUpdate()
+    {
+        m_rigidbody.drag = SO_Controller.dragDeceleration * SO_Controller.dragMultiplier;
+        ManageMove();
+    }
+
+    private void ManageMove()
+    {
+        var speed = m_timerDash <= 0 ? SO_Controller.m_speed : SO_Controller.m_dashSpeed;
+
+        int nbInputs = (Input.GetKey(KeyCode.Z) ? 1 : 0) + (Input.GetKey(KeyCode.Q) ? 1 : 0) +
+                       (Input.GetKey(KeyCode.S) ? 1 : 0) + (Input.GetKey(KeyCode.D) ? 1 : 0);
+        if (nbInputs > 1) speed *= 0.75f;
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            m_rigidbody.AddForce(Vector2.up*speed);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            m_rigidbody.AddForce(Vector2.left*speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_rigidbody.AddForce(Vector2.down*speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_rigidbody.AddForce(Vector2.right*speed);
+        }
+    }
+}
---
+++ SI/Assets/Scripts/SO_Controller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "PlayerController", menuName = "ScriptableObjects/new Player Controller", order = 1)]
+
+public class SO_Controller : ScriptableObject
+{
+    [Header("Mouvement")]
+    public float m_speed;
+    public float m_dashSpeed = 750f;
+    public float m_durationDash = 0.35f;
+    public float dragDeceleration = 12f;
+    public float dragMultiplier = 12f;
+}
