f186c0ab75b3ca8a52c3abc09edd18356fd6110a
Thomas Mottet <t.mottet@rubika-edu.com>
2022-09-29 20:15:41 +0200

    Animations

--- SI/Assets/Scripts/enemy/Enemies.cs
+++ SI/Assets/Scripts/enemy/Enemies.cs
@@ -17,6 +17,8 @@ public class Enemies : MonoBehaviour
     private float[] a_damages = new [] {20f, 30f, 40f};
     private float[] a_size = new[] { 2.3f, 1.75f, 1f };
 
+    [SerializeField] private Animator animator;
+
     void Start()
     {
         int x = Random.Range(0, a_speed.Length);
@@ -27,6 +29,30 @@ public class Enemies : MonoBehaviour
         transform.localScale = Vector3.one * a_size[x];
 
         gameObject.GetComponent<AIPath>().maxSpeed = speed;
+    }
+
+    [SerializeField] private Rigidbody2D rb;
+
+    void Update()
+    {
+        if (rb.velocity.y > 0)
+        {
+            animator.SetBool("isGoingUp", true);
+        }
+        else
+        {
+            animator.SetBool("isGoingUp", false);
+        }
+
+        if (rb.velocity.x < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        
     }
 
     public void TakingDamages(float t_damages)
